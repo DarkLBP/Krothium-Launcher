c4c33de04df2ba2eb57a76d89faeebc2c3fd5669
DarkLBP <darklbp@outlook.com>
2016-08-29 20:25:53 +0200

    Release 1.1.0

--- src/kml/Constants.java
+++ src/kml/Constants.java
@@ -8,8 +8,8 @@ import java.net.URL;
  */
 
 public class Constants {
-    public static final int KERNEL_BUILD = 13;
-    public static final String KERNEL_BUILD_NAME = "1.0.3";
+    public static final int KERNEL_BUILD = 14;
+    public static final String KERNEL_BUILD_NAME = "1.1.0";
     public static final URL AUTHENTICATE_URL = Utils.stringToURL("https://mc.krothium.com/authenticate");
     public static final URL REFRESH_URL = Utils.stringToURL("https://mc.krothium.com/refresh");
     public static final URL VALIDATE_URL = Utils.stringToURL("https://mc.krothium.com/validate");
