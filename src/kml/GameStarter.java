4ffcb9d276bea6db7f048f4756d58fa4f6c0678f
DarkLBP <darklbp@outlook.com>
2016-09-20 16:46:06 +0200

    Added certificate trusting for trigger classes

--- src/kml/GameStarter.java
+++ src/kml/GameStarter.java
@@ -1,7 +1,23 @@
 package kml;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import kml.handlers.URLHandler;
 
 /**
@@ -11,6 +27,25 @@ import kml.handlers.URLHandler;
 public class GameStarter {
     public static void main(String[] args){
         System.out.println("GameStarter launcher with " + args.length + " arguments.");
+        try {
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            Path ksPath = Paths.get(System.getProperty("java.home"), "lib", "security", "cacerts");
+            keyStore.load(Files.newInputStream(ksPath), "changeit".toCharArray());
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            try (InputStream caInput = new BufferedInputStream(
+                WebLauncher.class.getResourceAsStream("/mc_server_cert.der"))) {
+                Certificate crt = cf.generateCertificate(caInput);
+                System.out.println("Added certificate for " + ((X509Certificate) crt).getSubjectDN());
+                keyStore.setCertificateEntry("DSTRootCAX3", crt);
+            }
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(keyStore);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, tmf.getTrustManagers(), null);
+            SSLContext.setDefault(sslContext);
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException | KeyManagementException e) {
+            System.out.println("Failed to trust mc.krothium.com certificate.\n" + e.getMessage());
+        }
         URL.setURLStreamHandlerFactory(new URLHandler());
         String mainClass = args[0];
         String[] gameArgs = new String[args.length - 1];
--- src/kml/ServerLauncher.java
+++ src/kml/ServerLauncher.java
@@ -1,12 +1,28 @@
 package kml;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 
 /**
  * @website https://krothium.com
@@ -15,6 +31,25 @@ import java.util.List;
 public class ServerLauncher{
     public static void load(File f, String[] args){
         System.out.println("Krothium Minecraft Launcher " + Constants.KERNEL_BUILD_NAME);
+        try {
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            Path ksPath = Paths.get(System.getProperty("java.home"), "lib", "security", "cacerts");
+            keyStore.load(Files.newInputStream(ksPath), "changeit".toCharArray());
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            try (InputStream caInput = new BufferedInputStream(
+                WebLauncher.class.getResourceAsStream("/mc_server_cert.der"))) {
+                Certificate crt = cf.generateCertificate(caInput);
+                System.out.println("Added certificate for " + ((X509Certificate) crt).getSubjectDN());
+                keyStore.setCertificateEntry("DSTRootCAX3", crt);
+            }
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(keyStore);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, tmf.getTrustManagers(), null);
+            SSLContext.setDefault(sslContext);
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException | KeyManagementException e) {
+            System.out.println("Failed to trust mc.krothium.com certificate.\n" + e.getMessage());
+        }
         try{ 
             String r = Utils.sendPost(Constants.GETLATEST_URL, new byte[0], new HashMap());
             String[] data = r.split(":");
