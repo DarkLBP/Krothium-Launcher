78117e49090c3ee64d6400d469d03d83ae2f7ad3
DarkLBP <darklbp@outlook.com>
2016-10-01 18:43:27 +0200

    Added support for opening URLs with _blank target on external web browser

--- src/kml/objects/Browser.java
+++ src/kml/objects/Browser.java
@@ -1,11 +1,28 @@
 package kml.objects;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
 import javafx.scene.layout.Region;
+import javafx.scene.web.PopupFeatures;
 import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebEvent;
 import javafx.scene.web.WebView;
+import javafx.util.Callback;
 import kml.Constants;
+import kml.Utils;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.events.Event;
+import org.w3c.dom.events.EventListener;
+import org.w3c.dom.events.EventTarget;
+
+import java.io.IOException;
 
 /**
  * @website https://krothium.com
@@ -18,6 +35,38 @@ public class Browser extends Region {
 
     public Browser() {
         webEngine.setJavaScriptEnabled(true);
+        webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
+            @Override
+            public void changed(ObservableValue<? extends Worker.State> observable, Worker.State oldValue, Worker.State newValue) {
+                if (newValue == Worker.State.SUCCEEDED){
+                    EventListener listener = new EventListener() {
+                        @Override
+                        public void handleEvent(final Event event) {
+                            try {
+                                Utils.openWebsite(event.getTarget().toString());
+                            } catch (IOException e) {
+                                System.out.println("Failed to open url. " + event.getTarget());
+                            }
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                    };
+                    final Document document = webEngine.getDocument();
+                    if (document != null){
+                        NodeList list = document.getElementsByTagName("a");
+                        for (int i = 0; i < list.getLength(); i++){
+                            Node node = list.item(i);
+                            if (node instanceof EventTarget) {
+                                Node a = node.getAttributes().getNamedItem("target");
+                                if (a != null && a.getNodeValue().equalsIgnoreCase("_blank")){
+                                    ((EventTarget)node).addEventListener("click", listener, false);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
         webEngine.load("http://localhost:" + Constants.USED_PORT);
         getChildren().add(browser);
     }
