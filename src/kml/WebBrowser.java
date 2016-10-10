e3641351d940d256c2e6003aca7dc25390a2a1c1
DarkLBP <darklbp@outlook.com>
2016-10-10 17:04:00 +0200

    Decreased resolution and added a way to save the set resolution

--- src/kml/WebBrowser.java
+++ src/kml/WebBrowser.java
@@ -13,17 +13,36 @@ import kml.objects.Browser;
  */
 
 public class WebBrowser extends Application {
+    private Scene scene;
     @Override
     public void start(Stage stage) {
         stage.setTitle("Krothium Minecraft Launcher " + Constants.KERNEL_BUILD_NAME);
-        Browser browser = new Browser();
-        Scene scene = new Scene(browser, 1100, 750, Color.web("#666970"));
+        String width = WebLauncher.kernel.getProperty("width");
+        String height = WebLauncher.kernel.getProperty("height");
+        double w = 1050;
+        double h = 600;
+        if (width != null && height != null){
+            try {
+                w = Double.parseDouble(width);
+                h = Double.parseDouble(height);
+                if (w <= 0 || h <= 0){
+                    w = 1050;
+                    h = 600;
+                }
+            } catch (Exception ex){
+                w = 1050;
+                h = 600;
+            }
+        }
+        scene = new Scene(new Browser(), w, h, Color.web("#666970"));
         stage.getIcons().add(new Image(WebBrowser.class.getResourceAsStream("/icon.png")));
         stage.setScene(scene);
         stage.show();
     }
     @Override
     public void stop(){
+        WebLauncher.kernel.setProperty("width", String.valueOf(scene.getWidth()));
+        WebLauncher.kernel.setProperty("height", String.valueOf(scene.getHeight()));
         WebLauncher.kernel.exitSafely();
     }
 }
