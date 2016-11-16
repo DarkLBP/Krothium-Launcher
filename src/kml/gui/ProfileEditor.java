a1af5f6c54d9ca272b76000cb31423cc1ceaf5c0
DarkLBP <darklbp@outlook.com>
2016-11-16 18:47:14 +0100

    Profile Editor GUI structure

---
+++ src/kml/gui/ProfileEditor.java
@@ -0,0 +1,26 @@
+package kml.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by darkl on 16/11/2016.
+ */
+public class ProfileEditor {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JCheckBox gameDirectoryCheckBox;
+    private JTextField textField2;
+    private JCheckBox resolutionCheckBox;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JCheckBox enableExperimentalDevelopmentVersionsCheckBox;
+    private JCheckBox allowUseOfOldCheckBox;
+    private JCheckBox allowUseOfOldCheckBox1;
+    private JComboBox comboBox1;
+    private JTextField textField5;
+    private JCheckBox executableCheckBox;
+    private JCheckBox JVMArgumentsCheckBox;
+    private JPasswordField passwordField1;
+    private JButton saveProfileButton;
+    private JButton cancelButton;
+}
