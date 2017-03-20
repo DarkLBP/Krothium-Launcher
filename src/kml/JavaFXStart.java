c4328f69d869e369689db7607b0801ae3c9a7927
Benjamin Forrás <rsnconfigs@gmail.com>
2017-03-20 19:55:19 +0100

    Added fxml file and Minecraftia Font

--- src/kml/JavaFXStart.java
+++ src/kml/JavaFXStart.java
@@ -1,10 +1,11 @@
 package kml;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import kml.gui.MainFX;
 
 /**
  * Created by darkl on 17/03/2017.
@@ -17,13 +18,23 @@ public class JavaFXStart extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        MainFX main = new MainFX();
-        Scene scene = new Scene(main, 950, 700);
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/kml/gui/fxml/Main.fxml"));
+        Parent root = loader.load();
+
+        String style = getClass().getResource("/kml/gui/Main.css").toExternalForm();
+        root.getStylesheets().addAll(style);
+        primaryStage.getIcons().add(new Image("/kml/gui/textures/icon.png"));
+
+        primaryStage.setTitle("Krothium Minecraft Launcher " + Constants.KERNEL_BUILD_NAME);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+        /*Scene scene = new Scene(main, 950, 700);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Krothium Minecraft Launcher " + Constants.KERNEL_BUILD_NAME);
         primaryStage.getIcons().add(new Image("/kml/gui/textures/icon.png"));
 
-        primaryStage.show();
-
+        primaryStage.show();*/
     }
 }
--- src/kml/gui/MainFX.java
+++ src/kml/gui/MainFX.java
@@ -1,76 +1,37 @@
 package kml.gui;
 
-import javafx.geometry.Pos;
-import javafx.scene.Cursor;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
+import javafx.scene.control.ProgressBar;
 
 /**
  * Created by darkl on 18/03/2017.
  */
-public class MainFX extends VBox {
-    public MainFX() {
-        initialize();
-    }
+public class MainFX {
 
-    private void initialize() {
+    /**
+     * HEADER
+     */
+    @FXML
+    public Label languages;
+    @FXML
+    public Label switchAccount;
 
-        //Import stylesheet
-        String style = getClass().getResource("/kml/gui/Main.css").toExternalForm();
-        super.getStylesheets().addAll(style);
+    /**
+     * FOOTER
+     */
+    @FXML
+    public Label playButton;
+    @FXML
+    public Label profileButton;
+    @FXML
+    public ProgressBar progress;
 
-        //Header
-        VBox header = new VBox();
-        header.getStyleClass().add("darkBackground");
-        header.setMinHeight(180);
-        header.setAlignment(Pos.TOP_CENTER);
-        VBox.setVgrow(header, Priority.NEVER);
-        super.getChildren().addAll(header);
-
-        //Quick action row
-        StackPane actionRow = new StackPane();
-        Label languages = new Label("Languages");
-        languages.setCursor(Cursor.HAND);
-        languages.setId("languages");
-        languages.setMinHeight(35);
-        languages.setMaxHeight(35);
-        StackPane.setAlignment(languages, Pos.TOP_LEFT);
-        Label switchAccount = new Label("Switch Account");
-        switchAccount.setCursor(Cursor.HAND);
-        switchAccount.setId("switchAccount");
-        switchAccount.setMinHeight(35);
-        switchAccount.setMaxHeight(35);
-        StackPane.setAlignment(switchAccount, Pos.TOP_RIGHT);
-        actionRow.getChildren().addAll(languages, switchAccount);
-
-
-        //Logo row
-        ImageView logoRow = new ImageView();
-        logoRow.setImage(new Image("/kml/gui/textures/logo.png"));
-
-
-        header.getChildren().addAll(actionRow, logoRow); //Add everything to header
-
-
-        //Content
-        ScrollPane content = new ScrollPane();
-        content.getStyleClass().add("lightBackground");
-        VBox.setVgrow(content, Priority.ALWAYS);
-        content.setMinHeight(0);
-        super.getChildren().addAll(content);
-
-
-        //Footer
-        VBox footer = new VBox();
-        footer.getStyleClass().add("darkBackground");
-        footer.setMinHeight(150);
-        VBox.setVgrow(footer, Priority.NEVER);
-        super.getChildren().addAll(footer);
-
-
-        super.setPrefSize(950, 700);
+    public void initialize()
+    {
+        /*
+          Set progressbar width.
+         */
+        progress.setMaxWidth(Double.MAX_VALUE);
     }
 }
