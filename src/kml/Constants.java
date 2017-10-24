ceb0a27a07ae0937b13d1cd55a6bb1416ccda27e
DarkLBP <darklbp@outlook.com>
2017-10-24 07:29:25 +0200

    Release 3.1.1

--- src/kml/Constants.java
+++ src/kml/Constants.java
@@ -8,8 +8,8 @@ import java.io.File;
  */
 
 public final class Constants {
-    public static final int KERNEL_BUILD = 45;
-    public static final String KERNEL_BUILD_NAME = "3.1.0";
+    public static final int KERNEL_BUILD = 46;
+    public static final String KERNEL_BUILD_NAME = "3.1.1";
     public static final int KERNEL_FORMAT = 20;
     public static final int KERNEL_PROFILES_FORMAT = 2;
     public static final File APPLICATION_WORKING_DIR = Utils.getWorkingDirectory();
